9a53fa7de224b297c7c426d1e5b868cb84480c28
michaelhernandeza123-byte <michael.hernandez.a.123@gmail.com>
2026-09-12 20:40:49 -0400

    Add files via upload

---
+++ Week-01/hello.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+    int num1{0};
+    int num2{0};
+    int sum{0}; 
+    
+    std::cout << "Enter first integer: ";
+    std::cin >> num1;
+    std::cout << num2 << std::endl;
+    
+    std::cout << "Enter second integer: ";
+    std:: cin >> num2;
+
+    std::cout << num2 << std::endl; 
+    
+    sum = num1 + num2;
+    
+    std::cout << "sum is: " << sum << std::endl;
+    
+    
+}
